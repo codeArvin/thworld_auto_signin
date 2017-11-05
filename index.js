7e0913e3a7009ed01ed30bd3de7c2370c72b8823
codearvin <d51279679@163.com>
2017-11-05 23:56:24 +0800

    set task time

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 const schedule = require('./utils/schedule.js');
 const auto_sign_puppeteer = require('./auto_sign_puppeteer');
 
-// schedule.runTaskDaily('*:*:10', auto_sign_puppeteer);
-
-function task () {
-  console.log('11111');
-}
-
-schedule.runTaskDaily('*:*:*', task);
+schedule.runTaskDaily('16:30:00', auto_sign_puppeteer);
